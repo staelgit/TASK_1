6f0ddf4ee7be9c0dca685f907bb9e4c26556327f
styura3000 <styura3000@gmail.com>
2022-06-18 19:28:10 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import getRandomColor from './utils';
+
+export default function initApp() {
+  console.log('Hello world');
+
+  const button = document.createElement('button');
+  button.className = 'button';
+  button.textContent = 'Изменить цвет страницы';
+
+  document.body.append(button);
+
+  button.addEventListener('click', (event) => {
+    document.body.style.backgroundColor = getRandomColor();
+  });
+}
